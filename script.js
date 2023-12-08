fb3212272a8afc1626b4bad8ed7f13d3673f9a3f
Dima <goroshko5555@gmail.com>
2023-12-08 14:21:17 +0200

    comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let numberOfFilms = +prompt('How much films did u watch?');
+let numberOfFilms = +prompt('How much films did u watch?'); // Создаю переменную и ввожу в нее промтом значение
 
-const personalMovieDb = {
+const personalMovieDb = {     // создаю обьект со свойствами, в каунт передал значение с перемеенной выше
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
@@ -10,20 +10,20 @@ const personalMovieDb = {
 	private: false
 };
 
-const lastWatchMovie = prompt("One of the last movies"),
+const lastWatchMovie = prompt("One of the last movies"),             // ввожу значения для свойства-обьекта movies
      movieGrade = prompt("your grade"),
      secondWatchMovie = prompt("One of the last movies"),
      secondMovieGrade = prompt("your grade");
 
-// personalMovieDb['movies'] = {
+// personalMovieDb['movies'] = {                          // первый вариант как пеередать
 //     'lastWatchMovie': lastWatchMovie,
 //     'movieGrade': movieGrade,
 //     'secondWatchMovie':secondWatchMovie,
 //     'secondMovieGrade': secondMovieGrade
 // }
 
-personalMovieDb.movies[lastWatchMovie] = movieGrade;
+personalMovieDb.movies[lastWatchMovie] = movieGrade;    // второй
 personalMovieDb.movies[secondWatchMovie] = secondMovieGrade;
 
 
-console.log(personalMovieDb.movies);
+console.log(personalMovieDb.movies);  // вывод в консоль
