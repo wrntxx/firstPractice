98ec8057bccb16f6020fd3accf0b1aceaeb17b54
Dima <goroshko5555@gmail.com>
2023-12-08 14:03:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let numberOfFilms = prompt('How much films did u watch?');
+
+const personalMovieDb = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false
+};
+
+
