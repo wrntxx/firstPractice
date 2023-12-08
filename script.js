da449c6c5502a929e030e9646a579a3ca5912cc0
Dima <goroshko5555@gmail.com>
2023-12-08 14:15:32 +0200

    final result

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let numberOfFilms = prompt('How much films did u watch?');
+let numberOfFilms = +prompt('How much films did u watch?');
 
 const personalMovieDb = {
 	count: numberOfFilms,
@@ -10,4 +10,17 @@ const personalMovieDb = {
 	private: false
 };
 
+const lastWatchMovie = prompt("One of the last movies"),
+     movieGrade = prompt("your grade"),
+     secondWatchMovie = prompt("One of the last movies"),
+     secondMovieGrade = prompt("your grade");
 
+personalMovieDb['movies'] = {
+    'lastWatchMovie': lastWatchMovie,
+    'movieGrade': movieGrade,
+    'secondWatchMovie':secondWatchMovie,
+    'secondMovieGrade': secondMovieGrade
+}
+
+
+console.log(personalMovieDb.movies);
